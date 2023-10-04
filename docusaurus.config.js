514906a5c87889a27517361a3d8d5ca2b5f7a8bb
wgywmm <wgywmm@gmail.com>
2023-10-04 22:55:41 +0800

    edit config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   projectName: 'developer-roadmap', // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
